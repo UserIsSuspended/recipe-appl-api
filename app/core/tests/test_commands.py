f1f6d0abedea9a912cd52d3733fee440c7c0c73d
UserIsSuspended <2022.gamingacount.2022@gmail.com>
2024-03-02 23:53:37 +0200

    fixing linting

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch("core.management.commands.wait_for_db.Command.check")
 class CommandTests(SimpleTestCase):
     """Test commands"""
@@ -19,19 +20,21 @@ class CommandTests(SimpleTestCase):
 
         call_command("wait_for_db")
 
-        patched_check.assert_called_once_with(database=["default'"])
+        patched_check.assert_called_once_with(databases=["default"])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for db when getting an operational error"""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
             [OperationalError] * 3 + [True]
-        # the first 2 times of running this mocked function Psycopg2Error will be raised
+        # the first 2 times of running this mocked function Psycopg2Error
+        # will be raised
         # from times 3 to 5 django OperationalError will be raised in this mock
-        # the 6th time it returns True 
-        # backslash is the syntax to break it into 2 lines so it migth look like:
-        #pathced_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        # the 6th time it returns True
+        # backslash is the syntax to break it into 2 lines so it might look:
+        # pathced_check.side_effect = [Psycopg2Error] * 2 +
+        # [OperationalError] * 3 + [True](last 2  comms are 1 line)
 
         call_command("wait_for_db")
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=["default"])
+        patched_check.assert_called_with(databases=["default"])
