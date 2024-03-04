d90827488f6e5a8e95597cafceb475d8a0b38e7b
UserIsSuspended <2022.gamingacount.2022@gmail.com>
2024-03-04 16:40:58 +0200

    adding drf, drf_specrtacular and swagger

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,14 @@ class ModelTests(TestCase):
         """Test that creating a user without an email raises a ValueError"""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """TEst creating a supereuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+        
