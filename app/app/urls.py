383767cc3555ab23712a63d63ca4c085bd52acb8
UserIsSuspended <2022.gamingacount.2022@gmail.com>
2024-03-04 20:28:58 +0200

    tests added

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from drf_spectacular.views import (
-    SpectacularAPIVew,
+from drf_spectacular.views import(
+    SpectacularAPIView,
     SpectacularSwaggerView,
 )
 from django.contrib import admin
@@ -22,7 +22,7 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIVew.as_view(), name='api-schema'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/',
          SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
